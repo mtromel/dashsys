39b0a88ccd6df3301eb75d72c5e42cc686b108cc
Marcos Tromel <marcos.tromel@gmail.com>
2026-01-22 15:21:35 -0300

    correção do ALLOWED_HOSTS no settings.py

--- dashsys/settings.py
+++ dashsys/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.getenv('DEBUG')) == '1'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
 
 
 # Application definition
