af8fd2379864cb8735342e6bd235c8bbdf9d3fcd
Marcos Tromel <marcos.tromel@gmail.com>
2026-01-22 16:54:41 -0300

    corrigido visualização do dashboard por causa da mudança para ForeignKey

--- dash/views.py
+++ dash/views.py
@@ -3,6 +3,6 @@ from .models import Maquina
 
 def dashboard_pcp(request):
     # Buscamos todas as máquinas, trazendo junto o apontamento para ganhar performance
-    maquinas = Maquina.objects.select_related('apontamento').all().order_by('data_entrega')
+    maquinas = Maquina.objects.prefetch_related('apontamento').all().order_by('data_entrega')
     
     return render(request, 'dashboard.html', {'maquinas': maquinas})
