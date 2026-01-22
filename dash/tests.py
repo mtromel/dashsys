8ec2709b5f231070dc48feacc502cd649d74f8f9
Marcos Tromel <marcos.tromel@gmail.com>
2026-01-22 14:56:26 -0300

    correction of merge

--- dash/tests.py
+++ dash/tests.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 from django.test import TestCase
 
 # Create your tests here.
-=======
-from django.test import TestCase
-
-# Create your tests here.
->>>>>>> 9b7cea0e7e74c8caa9ed511e8ec74385db16918d
