ba86bbd478594fb4e1f5e7fa76201cef507bddd4
Marcos Tromel <marcos.tromel@gmail.com>
2026-04-17 10:51:50 -0300

    adiciona configuração de banco de dados para desenvolvimento e produção

--- dashsys/settings.py
+++ dashsys/settings.py
@@ -54,16 +54,29 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'dashsys.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST'),
-        'PORT': os.getenv('POSTGRES_PORT'),
+DEBUG = str(os.getenv('DEBUG')) == '1'
+
+
+if DEBUG:
+    # Use SQLite for develpment
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    # Use PostgreSQL for production
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('POSTGRES_DB'),
+            'USER': os.getenv('POSTGRES_USER'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+            'HOST': os.getenv('POSTGRES_HOST'),
+            'PORT': os.getenv('POSTGRES_PORT'),
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
